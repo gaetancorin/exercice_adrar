4149146b06d375ac6dc418f91e44451c6eb50d81
gaetan <gaetancorin@hotmail.fr>
2022-03-29 16:00:07 +0200

    update exo django des diapos

--- django/exo_diapo/monsite/sondage/models.py
+++ django/exo_diapo/monsite/sondage/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 import datetime
 
 # Create your models here.
@@ -11,7 +12,10 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -20,3 +24,7 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Citizen(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    electeur = models.BooleanField(default=False)
--- django/exo_diapo/monsite/sondage/urls.py
+++ django/exo_diapo/monsite/sondage/urls.py
@@ -3,8 +3,15 @@ from django.urls import path
 from . import views
 app_name = 'sondage'
 urlpatterns = [
-    path('', views.IndexViews.as_view(), name='index'),
-    path('<int:pk>/', views.DetailViews.as_view(), name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('login/', views.my_login, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.my_logout, name='logout'),
+    path('registered/', views.registered, name='registered'),
+    path('welcome/', views.welcome, name='welcome'),
+    path('profil/', views.profil, name='profil'),
+    path('profil/become', views.become, name='become'),
 ]
