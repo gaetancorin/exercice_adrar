55212af5ca8b6cd12dbff1d21db3300ebbf98504
gaetancorin <gaetancorin@gmail.com>
2022-07-22 14:58:15 +0200

    creation fichier angular avec premier component

---
+++ angular/snapface/src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  snaps! : number;
+}
