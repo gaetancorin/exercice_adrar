342e7153fc6bb707f54123e266631d9569420a55
gaetancorin <gaetancorin@gmail.com>
2022-07-22 17:08:28 +0200

    angular implémentation des data binding

--- angular/snapface/src/app/face-snap/face-snap.component.ts
+++ angular/snapface/src/app/face-snap/face-snap.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit{
   title!: string;
   description!: string;
   createDate!: Date;
   snaps! : number;
+  imageUrl! : string;
+
+  ngOnInit(){
+    this.title = "Mes vacances à la mer";
+    this.description = "On a mangé des glaces";
+    this.createDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = "https://auxmerveilleux.com/city/440/croissant.jpg";
+  }
 }
