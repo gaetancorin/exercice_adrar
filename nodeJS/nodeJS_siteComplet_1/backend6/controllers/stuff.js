d8d9e96be78d4b064f6e6355ebc9d23c7e1ab5ac
gaetancorin <gaetancorin@gmail.com>
2022-07-26 10:19:50 +0200

    nodejs création backend6

---
+++ nodeJS/nodeJS_siteComplet_1/backend6/controllers/stuff.js
@@ -0,0 +1,40 @@
+const Thing = require('../models/thing')
+
+//recoit un thing par post que l'on ajoute à MongoDb et rand une réponse. On supprime le paramètre ID puisque mongoDb le recrée par défault(risque d'erreur).
+exports.createThing = (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+    }
+
+// recoit des éléménets dans une requête put avec  dans l'url un ID en parametre, cherche dans MongoDb un élément avec le même ID, puis le modifie avec les nouveaux éléments en vérifiant à nouveau l'ID. 
+exports.modifyThing = (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, 
+    { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error })); 
+    }
+
+// recoit une requête delete avec un ID dans l'url, cherche dans MongoDb un élément avec le même ID, puis le supprime
+exports.deleteThing = (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
+    }
+// recoit une requête get avec un ID dans l'url, récupère le bon élément dans MongoDb et envois l'élément en réponse
+exports.getOneThing = (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+      .then(thing => res.status(200).json(thing))
+      .catch(error => res.status(404).json({ error }));
+  }
+
+// recoit une requête, récupère tous les éléments dans mongoDb et envois les éléments en réponse.
+exports.getAllThings = (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json((things)))
+        .catch(error => res.status(400).json({ error }));    
+    }
---
+++ nodeJS/nodeJS_siteComplet_1/backend6/routes/stuff.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const auth = require('../middleware/auth');
+const router = express.Router();
+
+
+const stuffCtrl = require('../controllers/stuff');
+
+router.post('/', auth, stuffCtrl.createThing);
+router.put('/:id', auth, stuffCtrl.modifyThing)
+router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.get('/', auth, stuffCtrl.getAllThings);
+
+module.exports = router;
