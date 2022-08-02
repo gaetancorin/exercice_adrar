5718721e4bdc219d7693a308d3604d25b42c6f98
gaetancorin <gaetancorin@gmail.com>
2022-08-02 20:39:28 +0200

    javascript firebase 25.0 explication et simplification terminé

--- javascript/FIREBASE/exo25.0_firebase_read/firebase25.0.js
+++ javascript/FIREBASE/exo25.0_firebase_read/firebase25.0.js
@@ -11,13 +11,13 @@ firebase.initializeApp(firebaseConfig);
 
 // On va créer une référence à notre BDD
 const dbRef = firebase.database().ref();
-//on va chercher les informations de la table "users" dans la BDD, on les stocks dans la variable sous forme d'objet database.
+//on va chercher les informations de la table "users" dans la BDD, on les stocks dans la variable sous forme d'objet "database".
 const usersRef = dbRef.child("users");
 
 readUserData();
 
 function readUserData(){
-  //récupération de la liste (vide pour le moment)
+  //récupération de l'élément ul 'user-list' (vide pour le moment)
   const userList = document.getElementById('user-list');
 //".on" est un "addEventListener" en Jquery.
 // on écoute la variable "usersRef" jusqu'a ce qu'il y ai des valeurs ou un changement de valeur,
@@ -26,21 +26,21 @@ function readUserData(){
     //On vide le contenu précédent
     userList.innerHTML = "";
     // snap est encore un objet database.
-    // On va parcourir snap (table users)
-    // childSnap va représenter chaque user sous forme d'objet database
+    // On va parcourir snap (qui est la table users)
+    // on sépare chaque snap en childSnap qui représente chaque user sous forme d'objet database
     snap.forEach(childSnap =>{ 
       //On stock la key pour chaque user
       let key = childSnap.key;
-      //On stock les valeurs correspondantes(email, name, age)
+      //On stock les valeurs correspondantes(email, name, age) sous forme de dictionnaire
       let value =childSnap.val()
       console.log(value);
       //creation de la <li></li>
       let $li = document.createElement("li");
       //remplissage de la <li></li>
       $li.innerHTML = value.name;
-      //On place un attribut user-key sur chaque <li></li>(qui est la key de la personne dans la BDD)
+      //On place un attribut user-key sur chaque <li></li>(qui prend comme valeur la key de l'utilisateur dans la BDD)
       $li.setAttribute("user-key", key);
-      //on place dans la liste <ul></ul> les <li> </li> créees
+      //on place dans la liste <ul></ul>'user-list' le <li></li> créé
       userList.append($li);})
 
   })
