82bac3e7a3987bea32fc551cde9dcf5a7707400f
gaetancorin <gaetancorin@gmail.com>
2022-08-02 10:27:12 +0200

    javascript firebase_simple changemnt date dans firebase pour bdd qui fonctionne, et changement nom dossier

--- javascript/firebase_simple/index.js
+++ javascript/firebase_simple/index.js
@@ -1,3 +1,5 @@
+// Se connecte a firebase "firebasetutosimple" "realtime database" sans utiliser npm ni cdn.
+// C'est l'ancienne maniere de se connecter a firebase, maintenant, ils forcent l'utilisation de npm
 const firebaseConfig = {
   apiKey: "AIzaSyDU5Drjn-nlVLIn_dQwCiodn8Ynt0wVlKE",
   authDomain: "fir-tutosimple.firebaseapp.com",
@@ -9,6 +11,7 @@ const firebaseConfig = {
   measurementId: "G-E0L3H7N29J"
 };
 firebase.initializeApp(firebaseConfig);
+// (je ne sais plus a quoi correspond firestore)
 // var db = firebase.firestore();
 
 // On stock dans une variable la reférence à notre BDD
