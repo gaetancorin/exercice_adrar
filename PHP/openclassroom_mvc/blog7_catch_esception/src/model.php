5331e1aa677a649fde8a6c4efc44cc5688034258
gaetancorin <gaetancorin@gmail.com>
2022-08-08 00:53:20 +0200

    php openclassroom mvc ajout V7 gestion des try/catch

---
+++ PHP/openclassroom_mvc/blog7_catch_esception/src/model.php
@@ -0,0 +1,58 @@
+<?php
+
+function getPosts() {
+	// We connect to the database.
+	$database = dbConnect();
+	// We retrieve the 5 last blog posts.
+	$statement = $database->query(
+    	"SELECT 
+			id, title, content, 
+			DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
+		FROM 
+			posts 
+		ORDER BY creation_date DESC LIMIT 0, 5"
+	);
+	$posts = [];
+	while (($row = $statement->fetch())) {
+    	$post = [
+        	'title' => $row['title'],
+        	'french_creation_date' => $row['french_creation_date'],
+        	'content' => $row['content'],
+			'identifier' => $row['id'],
+    	];
+
+    	$posts[] = $post;
+	}
+
+	return $posts;
+}
+
+function getPost($identifier) {
+	$database = dbConnect();
+    $statement = $database->prepare(
+        "SELECT 
+			id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date 
+		FROM 
+			posts 
+		WHERE 
+			id = ?"
+    );
+    $statement->execute([$identifier]);
+ 
+    $row = $statement->fetch();
+    $post = [
+        'title' => $row['title'],
+        'french_creation_date' => $row['french_creation_date'],
+        'content' => $row['content'],
+		'identifier' => $row['id'],
+    ];
+ 
+    return $post;
+}
+
+function dbConnect(){
+
+	$database = new PDO('mysql:host=localhost;dbname=mvc_php_openclassroom;charset=utf8', 'root', '');
+
+	return $database;
+}
