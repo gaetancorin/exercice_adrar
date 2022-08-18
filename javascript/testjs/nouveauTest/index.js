7eac2ffb00bc978fc6f0be718df73f36764fc17e
gaetancorin <gaetancorin@gmail.com>
2022-08-18 09:10:08 +0200

    javascript modification du projet perso testjs qui se connecte a pythonflask du repository microservicepython

--- javascript/testjs/nouveauTest/index.js
+++ javascript/testjs/nouveauTest/index.js
@@ -1,24 +1,25 @@
 let getAllDrinks = async() => {
-     // promesse
-    let data = await fetch('http://localhost:3000/getAllDrinks');
-    // json
+     // requête http GET vers mon api de lecture de boissons
+    let data = await fetch('http://127.0.0.1:5000/getAllDrinks');
+    // transformation de la réponse en json
     let count = await data.json();
-    console.log(count)
-    let compteur = 0
-    // await console.log(count.length);
+    // pour chaque élément, récupération de tous les paramètres transmis
     await count.forEach( element => {
-      compteur += 1;
       let title = element.title;
       let price = (element.price/100);
       let description = element.description;
       let imageUrl = element.imageUrl;
       let idTitle = title.replace(/ /g, "_");
 
-      let newDiv = document.querySelector(" body > div");
+      //récupération dans la liste de div vide de la div qui contiendra la boisson
+      let newDiv = document.querySelector(" body div");
+      // création des id et attribute de cette div
       newDiv.className = "drink";
       newDiv.id = idTitle;
+      // création de la div img
       let image = document.createElement("img");
       image.className = idTitle;
+      //récupération de l'url de l'image dans l'attribut src de img
       image.setAttribute("src", imageUrl);
       newDiv.appendChild(image);
       otherDiv = document.createElement("div");
@@ -48,12 +49,12 @@ document.querySelectorAll(" body > div").forEach(item => {
     console.log(attribute);
 
     document.querySelectorAll(" body > div").forEach(item => {
-      let oldChild = item.remove(" body > div > img");
-      oldChild = item.remove(" body > div > div ");
+      item.remove(" body > div > img");
+      item.remove(" body > div > div ");
       
     });
     let getOneDrink = async() => {
-      let env = String('http://localhost:3000/getOneDrink/'+attribute)
+      let env = String('http://127.0.0.1:5000/getOneDrink/'+attribute)
       let data = await fetch(env);
       let element = await data.json();
       console.log(element);
@@ -63,28 +64,28 @@ document.querySelectorAll(" body > div").forEach(item => {
       let imageUrl = element[0].imageUrl;
       let idTitle = title.replace(/ /g, "_");
 
-      // let newDiv = document.querySelector(" body > div");
-      // newDiv.className = "drink";
-      // newDiv.id = idTitle;
-      // let image = document.createElement("img");
-      // image.className = idTitle;
-      // image.setAttribute("src", imageUrl);
-      // newDiv.appendChild(image);
-      // // otherDiv = document.createElement("div");
-      // // otherDiv.className = 'Content';
-      // // newDiv.appendChild(otherDiv);
-      // // let firstp = document.createElement("p");
-      // // firstp.innerText = title;
-      // // otherDiv.appendChild(firstp);
-      // // let secondp = document.createElement("p");
-      // // secondp.innerText = price;
-      // // otherDiv.appendChild(secondp);
-      // // let thirstp = document.createElement("p");
-      // // thirstp.innerText = description;
-      // // otherDiv.appendChild(thirstp);
-    
-      // // document.body.appendChild(newDiv);
-      
+      let newDiv = document.createElement("div");
+      newDiv.className = "onedrink";
+      newDiv.id = idTitle;
+      let image = document.createElement("img");
+      image.className = idTitle;
+      image.className = "oneimg";
+      image.setAttribute("src", imageUrl);
+      newDiv.appendChild(image);
+      otherDiv = document.createElement("div");
+      otherDiv.className = 'Content';
+      newDiv.appendChild(otherDiv);
+      let firstp = document.createElement("p");
+      firstp.innerText = title;
+      otherDiv.appendChild(firstp);
+      let secondp = document.createElement("p");
+      secondp.innerText = price;
+      otherDiv.appendChild(secondp);
+      let thirstp = document.createElement("p");
+      thirstp.innerText = description;
+      otherDiv.appendChild(thirstp);
+      document.body.appendChild(newDiv);
+
     }
     getOneDrink();
     
